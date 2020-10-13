1b47a4679ef817d28e35c2dd9b49dca820d57310
Morgan Simonsen <morgan.simonsen@storebrand.no>
2020-10-13 09:29:18 +0000

    new script

---
+++ script.ps1
@@ -0,0 +1,36 @@
+function Send-Message {
+    <#
+    .SYNOPSIS
+        This function send a message
+    .DESCRIPTION
+        Long description
+    .EXAMPLE
+        PS C:\> <example usage>
+        Explanation of what the example does
+    .INPUTS
+        Inputs (if any)
+    .OUTPUTS
+        Output (if any)
+    .NOTES
+        General notes
+    #>
+    [CmdletBinding()]
+    param (
+        # Message
+        [Parameter(Mandatory=$true)]
+        [string]
+        $Message
+    )
+    
+    begin {
+        
+    }
+    
+    process {
+        
+    }
+    
+    end {
+        
+    }
+}
